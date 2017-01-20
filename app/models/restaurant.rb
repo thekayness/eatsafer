49d35a227f843a463330bfb994468ab3cb72d1f8
Kathleen <kayjsmyth@gmail.com>
2017-01-20 11:55:54 -0500

    added methods to Restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,16 @@
 class Restaurant < ApplicationRecord
 	has_many :inspections
+
+	geocoded_by :full_street_address   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
+	
+	def self.find_by_name(search_terms)
+		where("name LIKE?", "%#{search_terms}%")
+	end
+
+	def full_address
+		if address
+			[address.squeeze(" "), city, state].compact.join(', ')
+		end
+	end
 end
