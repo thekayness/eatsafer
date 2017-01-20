958ca9bf7d437ad5e16b6120f82bd5aac1ad84f0
Kathleen <kayjsmyth@gmail.com>
2017-01-20 11:47:02 -0500

    added search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,11 @@
+class SearchController < ApplicationController
+	def name
+		@restaurants = Restaurant.find_by_name(params[:name])
+    	render json: @restaurants
+	end
+
+	def address
+	    @restaurants = Restaurant.near(params[:address], 0.5)
+	    render json: @restaurants		
+	end
+end
